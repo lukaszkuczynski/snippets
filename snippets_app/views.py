86bd166c9f168d69aea6c457c48ea023279e25db
kuczynskilukasz <kuczynskilukasz@gmail.com>
2017-06-22 18:38:31 +0200

    View is populated by Model

--- snippets_app/views.py
+++ snippets_app/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .models import Snippet
+
 def index(request):
-    return render(request, 'index.html')
+    snippets = Snippet.objects.all()
+    return render(request, 'index.html', context={"snippets": snippets})
